45bce0ec6cfb02595e037af721580465d463ff46
chaitu1 <chaitu143@gmail.com>
2024-06-06 22:40:23 +0530

    Ansible expense setup is ready

---
+++ functions/merge/main.tf
@@ -0,0 +1,13 @@
+variable "name" {
+  default = {
+    Name = "Roboshop-Project"
+    app  = "frontend"
+  }
+}
+
+output "merge-function" {
+  value = merge(var.name,{
+    company = "Thaught-works"
+    company1 = "sony-india"
+  })
+}
--- variables/variables-01/main.tf
+++ variables/variables-01/main.tf
@@ -1,23 +1,23 @@
-#resource "null_resource" "sample" {
-#  for_each  = var.fruits
-#  provisioner "local-exec" {
-#    command   = "echo  fruitname=${each.value["fruit_name"]} cost=${lookup(each.value,"cost",400)}"
-#  }
-#}
-#
-#resource "null_resource" "sample1" {
-#  for_each  = var.fruits
-#  provisioner "local-exec" {
-#    command   = "echo  fruitname=${var.fruits[each.key].fruit_name} cost=${var.fruits[each.key]["cost"]}"
-#  }
-#}
-#
-#resource "null_resource" "sample2" {
-#  for_each  = var.fruits
-#  provisioner "local-exec" {
-#    command   = "echo  fruitname=${lookup(lookup(var.fruits,each.key,"default"),"fruit_name","common")}"
-#  }
-#}
+resource "null_resource" "sample" {
+  for_each  = var.fruits
+  provisioner "local-exec" {
+    command   = "echo  fruitname=${each.value["fruit_name"]} cost=${lookup(each.value,"cost",400)}"
+  }
+}
+
+resource "null_resource" "sample1" {
+  for_each  = var.fruits
+  provisioner "local-exec" {
+    command   = "echo  fruitname=${var.fruits[each.key].fruit_name} cost=${var.fruits[each.key]["cost"]}"
+  }
+}
+
+resource "null_resource" "sample2" {
+  for_each  = var.fruits
+  provisioner "local-exec" {
+    command   = "echo  fruitname=${lookup(lookup(var.fruits,each.key,"default"),"fruit_name","common")}"
+  }
+}
 
 output "apple_cost" {
   value = var.fruits["apple"]["cost"]
