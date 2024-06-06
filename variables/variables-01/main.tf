a7e1dbe8d5de06e46c59f4b9f19762108e71f301
chaitu1 <chaitu143@gmail.com>
2024-06-06 22:12:28 +0530

    Ansible expense setup is ready

--- variables/variables-01/main.tf
+++ variables/variables-01/main.tf
@@ -1,7 +1,4 @@
 resource "null_resource" "sample" {
-  triggers = {
-    name = "hello1"
-  }
   for_each  = var.fruits
   provisioner "local-exec" {
     command   = "echo  fruitname=${each.value["fruit_name"]} cost=${lookup(each.value,"cost",400)}"
