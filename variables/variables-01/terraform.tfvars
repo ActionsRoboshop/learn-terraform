2073f21323c7192a96ee5ac0d52672d8b5f155af
chaitu1 <chaitu143@gmail.com>
2024-06-06 22:06:58 +0530

    Ansible expense setup is ready

---
+++ variables/variables-01/terraform.tfvars
@@ -0,0 +1,15 @@
+
+fruits = {
+  apple = {
+    fruit_name = "apple"
+    cost       = 100
+  }
+  banana = {
+    fruit_name = "banana"
+    cost       = 50
+  }
+  water-melon = {
+    fruit_name = "water-melon"
+    cost       = 50
+  }
+}
--- variables/variables-01/variables.tf
+++ variables/variables-01/variables.tf
@@ -1,16 +1,2 @@
 variable "fruits" {}
 
-fruits = {
-  apple = {
-    fruit_name = "apple"
-    cost       = 100
-  }
-  banana = {
-    fruit_name = "banana"
-    cost       = 50
-  }
-  water-melon = {
-    fruit_name = "water-melon"
-    cost       = 50
-  }
-}
